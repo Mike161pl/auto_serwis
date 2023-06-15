e68ebb25d9a015dd2286457a9c1569d5fece9ddf
mike161pl <mike161pl@gmail.com>
2023-06-15 20:50:50 +0200

    raefactoring

---
+++ src/js/slick.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.gallery__boxes').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        mobileFirst: true,
+        responsive: [{
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }],
+
+    });
+})
